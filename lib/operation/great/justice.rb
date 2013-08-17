67f69dccdae3a68daa4bac09b5c3a836df3481d7
Thong Kuah <kuahyeow@gmail.com>
2013-08-17 13:11:52 +1200

    Add -n flag for number mode

--- lib/operation/great/justice.rb
+++ lib/operation/great/justice.rb
@@ -17,9 +17,24 @@ module Operation
         first = (first.to_f / 0xffffffff * adjectives.size).to_i
         last = (last.to_f / 0xffffffff * nouns.size).to_i
 
-        # Collect our words
-        first = adjectives[first]
-        last  = nouns[last]
+        obtain(first, last)
+      end
+
+      def generate_from_number(token)
+        obtain(token.to_i)
+      end
+
+      def obtain(*args)
+        if args.size == 1
+          adjective_pos, noun_pos = calculate_positions(args.first)
+        elsif args.size == 2
+          adjective_pos, noun_pos = args
+        else
+          raise ArgumentError, "wrong number of arguments (#{args.size} for 1..2)"
+        end
+
+        first = adjectives[adjective_pos]
+        last  = nouns[noun_pos]
 
         # Capitalize our words
         first = first.slice(0,1).capitalize + first.slice(1..-1)
@@ -45,6 +60,13 @@ module Operation
 
       def adjectives
         @adjectives ||= File.read(File.expand_path('../../../../adjectives.txt', __FILE__)).split("\n")
+      end
+
+      def calculate_positions(position)
+        position      = position % (adjectives.size * nouns.size)
+        adjective_pos = position / nouns.size
+        noun_pos      = position - (adjective_pos * nouns.size) - 1
+        [adjective_pos, noun_pos]
       end
     end
   end
